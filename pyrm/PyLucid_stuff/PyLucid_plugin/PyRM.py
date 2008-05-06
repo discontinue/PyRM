314ba601643b7cb5161f9dfad964cdbba43a6a8a
JensDiemer <git@jensdiemer.de>
2008-05-06 10:54:48 +0000

    add some first methods

--- pyrm/PyLucid_stuff/PyLucid_plugin/PyRM.py
+++ pyrm/PyLucid_stuff/PyLucid_plugin/PyRM.py
@@ -18,18 +18,80 @@
 __version__= "$Rev: $"
 
 from django.utils.translation import ugettext as _
+from django import newforms as forms
 
 from PyLucid.system.BasePlugin import PyLucidBasePlugin
+
+from PyRM.models import Kunde, RechnungsPosition, Rechnung
+
+
+class CreateBillForm(forms.Form):
+    """
+    Form for creating a bill.
+    http://www.djangoproject.com/documentation/newforms/
+    """
+    rechnungnummer = forms.IntegerField()
+    bestellnummer = forms.IntegerField()
+    datum = forms.DateTimeField()
+
+    kunde = forms.ModelChoiceField(Konto.objects.all())
+    ggkto = forms.ModelChoiceField(Konto.objects.all())
+    kunde = forms.ModelChoiceField(Kunde.objects.all())
+
+    lieferdatum = forms.DateTimeField()
+
 
 class PyRM(PyLucidBasePlugin):
 
     def summary(self):
         """
+        Übersicht
         """
         # Change the global page title:
-        self.context["PAGE"].title = _("PyRM - summary")
+        self.context["PAGE"].title = _(u"PyRM - Übersicht")
 
-        self.page_msg("summary")
+        self.page_msg(u"Übersicht")
+
+    def customers(self):
+        """
+        Kunden
+        """
+        # Change the global page title:
+        self.context["PAGE"].title = _(u"PyRM - Kunden")
+
+        self.page_msg(u"Kunden")
+
+    def bills(self):
+        """
+        Rechnungen
+        """
+        # Change the global page title:
+        self.context["PAGE"].title = _(u"PyRM - Rechnungen")
+
+        self.page_msg(u"Rechnungen")
+
+    def create_bill(self):
+        """
+        Rechnung erstellen
+        """
+        # Change the global page title:
+        self.context["PAGE"].title = _(u"PyRM - Rechnung erstellen")
+
+        self.page_msg(u"Rechnung erstellen")
+
+
+
+        if self.request.method == 'POST':
+            self.page_msg(self.request.POST)
+            form = CreateBillForm(self.request.POST)
+#            if form.is_valid():
+#                # Do form processing here...
+#                return HttpResponseRedirect('/url/on_success/')
+        else:
+            form = CreateBillForm()
+
+        self.response.write(form.as_p())
+
 
 
 
--- pyrm/PyLucid_stuff/PyLucid_plugin/PyRM_cfg.py
+++ pyrm/PyLucid_stuff/PyLucid_plugin/PyRM_cfg.py
@@ -12,9 +12,14 @@ __long_description__ = """
 #_____________________________________________________________________________
 # plugin administration data
 
+global_rights = {
+    "must_login"    : False,
+    "must_admin"    : False,
+}
+
 plugin_manager_data = {
-    "summary" : {
-        "must_login"    : False,
-        "must_admin"    : False,
-    }
+    "summary" : global_rights,
+    "customers" : global_rights,
+    "bills" : global_rights,
+    "create_bill" : global_rights,
 }
