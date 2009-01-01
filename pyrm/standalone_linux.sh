815a022210c01442fc5138c9e38d2c45fe08ac7d
AlenHopek <git@hopek.de>
2009-01-01 10:10:54 +0000

    added System os check for Linux / MacOSX (Darwin) and detection of current ip adress for eth0 / en0

--- pyrm/standalone_linux.sh
+++ pyrm/standalone_linux.sh
@@ -3,8 +3,23 @@
 # use the local django packages
 export PYTHONPATH=${PWD}
 
-addr=`/sbin/ifconfig eth0 | awk '/inet Adr/ {split ($2,A,":"); print A[2]":8000"}'`
+PORT='8000'
+CHECK_DARWIN=`uname -a | awk '/Darwin/ {split ($1,A,":"); print A[1]}'`
+CHECK_LINUX=`uname  -a | awk '/Linux/  {split ($1,A,":"); print A[1]}'`
 
-echo 'Starting django development server...'
-python ./manage.py runserver ${addr} $*
+if [ ! -z $CHECK_DARWIN ]; then
+	echo 'found Darwin ...'
+	ADDR=`/sbin/ifconfig en0 | awk '/inet / {split ($2,A,":"); print A[1]}'`
+fi
 
+if [ ! -z $CHECK_LINUX ]; then
+	echo 'found Linux ...'
+	ADDR=`/sbin/ifconfig eth0 | awk '/inet Adr/ {split ($2,A,":"); print A[2]}'`
+fi
+
+if [ ! -z $ADDR ]; then
+	echo 'Starting django development server...'
+	python ./manage.py runserver ${ADDR}":$PORT" $*
+else
+	echo 'can not detect your IP-Adress'
+fi
