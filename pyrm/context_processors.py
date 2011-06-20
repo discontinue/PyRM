fbb42aa7e094fc81553d3685823c7eb83d568325
JensDiemer <git@jensdiemer.de>
2011-06-20 14:05:22 +0200

    use blueprint

--- pyrm/context_processors.py
+++ pyrm/context_processors.py
@@ -5,6 +5,7 @@ from django.conf import settings
 
 def pyrm(request):
     context = {
+        "debug": settings.DEBUG,
         "media_url": settings.MEDIA_URL,
         "admin_url_prefix": settings.ADMIN_URL_PREFIX,
         "Django_media_prefix": settings.ADMIN_MEDIA_PREFIX,
---
+++ pyrm/static/blueprint/wget_update.sh
@@ -0,0 +1,3 @@
+wget --timestamp --no-check-certificate http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/screen.css
+wget --timestamp --no-check-certificate http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/print.css
+wget --timestamp --no-check-certificate http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/ie.css
