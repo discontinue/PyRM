0f12b4e4d0abc6732771297bd7391d451edcbbea
JensDiemer <git@jensdiemer.de>
2011-06-22 16:21:39 +0200

    add DATE_FORMAT in de scheme

--- testproject/settings.py
+++ testproject/settings.py
@@ -55,17 +55,6 @@ MANAGERS = ADMINS
 #______________________________________________________________________________
 # DATABASE SETUP
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Europe/Berlin'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'de-DE'
-
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = True
@@ -108,6 +97,7 @@ TEMPLATE_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(BASE_PATH, "templates"),
+    os.path.expanduser("~/pyrm_env/src/django-reversion/src/reversion/templates"),
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -155,6 +145,21 @@ DATABASES = {
     }
 }
 
+
+#_______________________________________________________________________________
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Berlin'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'de-DE'
+
+DATE_FORMAT = "%d.%m.%Y"
 
 #_______________________________________________________________________________
 
