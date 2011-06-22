ac47eff52e033ec651a0efe0530c5c4c47d8258c
JensDiemer <git@jensdiemer.de>
2011-06-22 16:40:58 +0200

    use only the 24 columns grid.css from blueprint.

--- pyrm/static/pyrm/blueprint/wget_update.sh
+++ pyrm/static/pyrm/blueprint/wget_update.sh
@@ -1,1 +1,1 @@
-wget --timestamp --no-check-certificate http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/screen.css
+wget --timestamp --no-check-certificate https://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/src/grid.css
