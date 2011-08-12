02c61b3e163f51d8c7ec7791bbb08434f2027d27
JensDiemer <git@jensdiemer.de>
2011-08-12 11:19:58 +0200

    Use creole in README, see also: https://code.google.com/p/python-creole/wiki/UseInSetup

--- setup.py
+++ setup.py
@@ -31,11 +31,21 @@ def get_authors():
 
 def get_long_description():
     try:
-        f = file(os.path.join(PACKAGE_ROOT, "README"), "r")
-        long_description = f.read().strip()
+        f = file(os.path.join(PACKAGE_ROOT, "README.creole"), "r")
+        desc_creole = f.read()
         f.close()
+
+        desc_creole = unicode(desc_creole, 'utf-8').strip()
+
+        from creole import creole2html, html2rest
+
+        desc_html = creole2html(desc_creole)
+        long_description = html2rest(desc_html)
     except Exception, err:
+        if len(sys.argv) > 1 and sys.argv[1] in ("--long-description", "sdist"):
+            raise
         long_description = "[Error: %s]" % err
+
     return long_description
 
 
